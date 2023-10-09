f2d7eda9bfec5f3965ec0d907fbaa8a7c8080dc8
Shitcoin <shitcoinproject0@gmail.com>
2023-10-08 21:16:58 -0300

    asdd

--- classes/Login.php
+++ classes/Login.php
@@ -67,7 +67,7 @@ class Login
 
                 // database query, getting all the info of the selected user (allows login via email address in the
                 // username field)
-                $sql = "SELECT id, username, mail, password_hash, rank
+                $sql = "SELECT id, username, mail, password_hash
                         FROM user
                         WHERE (username = '" . $user_name . "' OR mail = '" . $user_name . "');";
                 $result_of_login_check = $this->db_connection->query($sql);
