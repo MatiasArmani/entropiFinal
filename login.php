394a68f8cddcdeb0d29028978d67e27eb0c4344e
Shitcoin <shitcoinproject0@gmail.com>
2023-10-08 21:43:44 -0300

    asdsdf

--- login.php
+++ login.php
@@ -26,7 +26,6 @@ if ($login->isUserLoggedIn() == true) {
    header("location: home-logged.php");
 
 } else {
-   header("location: home-logged.php");
     // the user is not logged in. you can do whatever you want here.
     // for demonstration purposes, we simply show the "you are not logged in" view.
     ?>
