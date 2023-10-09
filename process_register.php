7888563c9db6cc20644d53cd0ec403e98cccda14
Shitcoin <shitcoinproject0@gmail.com>
2023-10-08 23:36:54 -0300

    sdfsdf

--- process_register.php
+++ process_register.php
@@ -1,18 +1,5 @@
 <?php
-	session_start();
-	if (!isset($_SESSION['user_login_status']) AND $_SESSION['user_login_status'] != 1) {
-        header("location: login.php");
-		exit;
-        }
-	
-	/* conect To Database*/
-	require_once ("config/db.php");//Contiene las variables de configuracion para conectar a la base de datos
-	require_once ("config/conexion.php");//Contiene funcion que conecta a la base de datos
-?>
-
-
-<?php
-//include 'database.php';
+include 'database.php';
 
 // Por simplicidad, no estoy conectando a una base de datos aquí. 
 // Deberías conectar y guardar los datos del usuario en la base de datos.
@@ -28,10 +15,10 @@ $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 $sql = "INSERT INTO User (username, mail, password_hash) VALUES ('$user_username', '$email', '$hashed_password')";
 
 
-if ($con->query($sql) === TRUE) {
+if ($conn->query($sql) === TRUE) {
     echo "New user registered successfully";
 } else {
-    echo "Error: " . $sql . "<br>" . $con->error;
+    echo "Error: " . $sql . "<br>" . $conn->error;
 }
 
 // Aquí guardarías el email y contraseña en la base de datos.
@@ -39,13 +26,13 @@ if ($con->query($sql) === TRUE) {
 if ($role == 'researcher') {
     
     $sqlinv = "INSERT INTO Investigator (username) VALUES ('$user_username')";
-    if ($con->query($sqlinv) === TRUE) {
+    if ($conn->query($sqlinv) === TRUE) {
     echo "New user registered successfully";
     }
     header("Location: registerinv.php?username=$user_username");
 } elseif ($role == 'organization') {
     $sqlorg = "INSERT INTO Organization (username) VALUES ('$user_username')";
-    if ($con->query($sqlorg) === TRUE) {
+    if ($conn->query($sqlorg) === TRUE) {
     echo "New user registered successfully";
     } 
     header("Location: registerorg.php?username=$user_username");
